7f58a7d26ca0505c0b5952cb44af1cb4c8684bfe
GBaris <126857159+GBaris@users.noreply.github.com>
2023-03-03 13:23:13 +0300

    viewmodel eklendi

---
+++ ULDeneme.ViewModel/QuizTestViewModels/asdasd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULDeneme.ViewModel.QuizTestViewModels
+{
+    internal class asdasd
+    {
+    }
+}
